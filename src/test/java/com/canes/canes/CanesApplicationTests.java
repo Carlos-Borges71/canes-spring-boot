09dce2d655c9aaa2c48fc845e896d532efa149e9
Carlos borges <kaca_master@hotmail.com>
2025-05-01 21:50:55 -0300

    Projeto Criado

---
+++ src/test/java/com/canes/canes/CanesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canes.canes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
